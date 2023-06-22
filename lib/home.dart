a44ddc3f6cb09e12d0123c68099647ac3138b9ed
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-06-22 12:10:51 +0530

    addded search bar and changed font in themeddata

--- lib/home.dart
+++ lib/home.dart
@@ -6,6 +6,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController searchContoller = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +24,71 @@ class _HomeState extends State<Home> {
               ),
             ),
           ),
-          SafeArea(child: Text("HELLO I AM HOME PAGE" ,style: TextStyle(color: Colors.white),)),
+        Column(
+          children: [
+            SafeArea(
+              child: Container(
+                // Search wala container
+                // color: Colors.grey,
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(24)),
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        if((searchContoller.text).replaceAll(" ", "") == ""){
+                          print("Blank search");
+                        }
+                        else{
+                          Navigator.pushReplacementNamed(context, "/loading",arguments: {
+                            "searchText" : searchContoller.text,
+                          });
+                        }
+
+                        print(searchContoller.text);
+                      },
+                      child: Container(
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.blueAccent,
+                        ),
+                        margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
+                      ),
+                    ),
+                    Expanded(
+                        child: TextField(
+                          controller: searchContoller,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Let's Cook Something",
+                          ),
+                        )
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+           Container(
+             padding: EdgeInsets.symmetric(horizontal:20 ),
+             child: Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: [
+                 Text("WHAT DO YOU WANT TO COOK TODAY ?", style: TextStyle(
+                   color: Colors.white, fontSize: 33,
+                 ),),
+                 SizedBox(height: 10,),
+                 Text("Let's Cook Something New!" , style: TextStyle(
+                   color: Colors.white, fontSize: 20,
+                 ),)
+               ],
+             ),
+           )
+    ],
+        )
 
 
         ],
--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
+        fontFamily: "Poppins",
         primarySwatch: Colors.blue,
       ),
       home: Home(),
